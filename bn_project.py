37054e2bf8adbd0e94e6cf1be0a7e77913822cc5
ross@bardai.com.au <ross@edcore.com.au>
2021-11-05 23:21:17 +1100

    updated netica libary, fixed nodes with no state names

--- bn_project.py
+++ bn_project.py
@@ -1,4 +1,4 @@
-import _env,csv,math 
+import _env,csv,math
 from bni_netica import *
 
 bn_model = 'progression5.trained.dne'
@@ -6,36 +6,80 @@ bn_model = 'progression5.trained.dne'
 nested_dictionary = { }
 
 def updateBn(param_dict):
-	print('Running model with output: {}'.format(param_dict), file=sys.stderr)
-	
-	net = Net(bn_model)
-	net.retractFindings()
-	#pass parameter dictionary to set evidence
-	output_test_string = "Values set: "
-	for node in net.nodes():
-		if node.name() in param_dict:
-			try:
-				print ('setting evidence for node: ' + node.name() + ' with value: ', param_dict[node.name()], file=sys.stderr)
-				enterFinding(node, param_dict[node.name()])
-				print ('output: ', node.beliefs(), file=sys.stderr)
-			except:
-				print ('    exception caught!', file=sys.stderr)
-				pass
-			for state in node.states():
-				print ('    state name: ' + state.name(), file=sys.stderr)
-			
-	#update output dictionary
-				
-	for node in net.nodes():
-		nested_dictionary[node.name()] = {}
-		index = 0
-		for state in node.states():
-			nested_dictionary[node.name()][state.name()] = node.beliefs()[index]
-			index+=1
-				
-	return nested_dictionary
-	
-	
+    print('Running model with output: {}'.format(param_dict), file=sys.stderr)
+
+    net = Net(bn_model)
+    net.retractFindings()
+    #pass parameter dictionary to set evidence
+    output_test_string = "Values set: "
+    for node in net.nodes():
+        if node.name() in param_dict:
+            try:
+                print ('setting evidence for node: ' + node.name() + ' with value: ', param_dict[node.name()], file=sys.stderr)
+
+                if node.name() == 'ci_pulse_rate_t0':
+                    print ('    Converting ci_pulse_rate_t0 to ci_func_car_t0')
+                    beats = param_dict[node.name()]
+                    status = ''
+                    if beats < 45 and beats > 199:
+                        status = 'abnormal'
+                    else:
+                        status = 'normal'
+                    #TODO Check with Yue, assume greater than 95 is low
+                    enterFinding(net.node('ci_func_car_t0'), status)
+                if node.name() == 'ci_oxygen_saturation_t0':
+                    print ('    Converting ci_oxygen_saturation_t0 to ci_hypoxaemia_t0')
+                    #If SaO2 is between 90-95%, 'ci_hypoxaemia_t0' is 'moderate'. If SaO2 is <=89%, 'ci_hypoxaemia_t0' is 'severe'.
+                    sao2 = param_dict[node.name()]
+                    status = ''
+                    if sao2 <= 0.89:
+                        status = 'severe'
+                    else:
+                        status = 'moderate'
+                        #TODO Check with Yue, assume greater than 95 is low
+                    enterFinding(net.node('ci_hypoxaemia_t0'), status)
+                if node.name() == 'CREATININE':
+                    print ('    CONVERT CRT INTEGER TO DISCRETISATION')
+                if node.name() == 'NEUTRO' and 'LYMPH':
+                    print ('    CALC to NLR and CONVERT NLR int to DISCRETISATION')
+                if node.name() == 'NEUTRO' and 'LYMPH':
+                    print ('    CALC to NLR and CONVERT NLR int to DISCRETISATION')
+                if node.name() == 'NEUTRO' and not 'LYMPH':
+                    print ('    CONVERT NEUTRO int to DISCRETISATION')
+                if node.name() == 'LYMPH' and not 'NEUTRO':
+                    print ('    CONVERT LYMPH int to DISCRETISATION')
+                else:
+                    enterFinding(node, param_dict[node.name()])
+                print ('output: ', node.beliefs(), file=sys.stderr)
+            except:
+                print ('    exception caught!', file=sys.stderr)
+                pass
+            for state in node.states():
+                print ('    state name: ' + state.name(), file=sys.stderr)
+
+    #add meta data: the bn name and version, to nested dictionary
+    #TODO remove this line and replace with user defined variable in the netica BN net_version
+    net.add_net_usr_field('net_version', '1')
+    #TODO remove this line and replace with user defined variable in the netica BN net_name
+    net.add_net_usr_field('net_name', 'progression model')
+    print ('API request made for BN name: ' + net.get_net_usr_field('net_name') + ' with version: ' + net.get_net_usr_field('net_version'))
+    nested_dictionary['model'] = {}
+    nested_dictionary['model']['name'] = net.get_net_usr_field('net_name')
+    nested_dictionary['model']['version'] = net.get_net_usr_field('net_version')
+    nested_dictionary['data'] = {}
+
+    #update output dictionary
+    for node in net.nodes():
+        nested_dictionary['data'][node.name()] = {}
+        index = 0
+        for state in node.states():
+            if state.name() == '':
+                nested_dictionary['data'][node.name()]['s'+str(index)] = node.beliefs()[index]
+            else:
+                nested_dictionary['data'][node.name()][state.name()] = node.beliefs()[index]
+            index+=1
+    return nested_dictionary
+
 def enterFinding(node, value):
     if node.type() == Node.DISCRETE_TYPE: node.finding(state=value)
     if node.type() == Node.CONTINUOUS_TYPE: node.finding(value=value)
--- db.py
+++ db.py
@@ -27,7 +27,7 @@ def run_import():
     
     #finally move the import json to export to prevent duplicate entries
     print("Moving the import JSON to: " + EXPORT_FOLDER + JSON_FILE)
-    shutil.move(json_path, EXPORT_FOLDER + JSON_FILE)
+    #shutil.move(json_path, EXPORT_FOLDER + JSON_FILE)
     print("IMPORT COMPLETE (SUCESS)")
 
 def create_connection(db_file):
@@ -56,7 +56,7 @@ def create_tables(db_file):
         print("creating table (if it does not exist): timeseries")
         cursor.execute('''    
         CREATE TABLE IF NOT EXISTS timeSeries (id integer primary key not null,
-            usubjid integer, day integer, ci_ho_type, ci_ds_decod, ci_Body_Mass_Index, ci_Body_Mass_Index_u, ci_Diastolic_Blood_Pressure, ci_Diastolic_Blood_Pressure_u, ci_Heart_Rate, ci_Heart_Rate_u, ci_Height, ci_Height_u, ci_Mean_Arterial_Pressure, ci_Mean_Arterial_Pressure_u, ci_Mid_Upper_Arm_Circumference, ci_Mid_Upper_Arm_Circumference_u, ci_Oxygen_Saturation, ci_Oxygen_Saturation_u, ci_Pulse_Rate, ci_Pulse_Rate_u, ci_Respiratory_Rate, ci_Respiratory_Rate_u, ci_Systolic_Blood_Pressure, ci_Systolic_Blood_Pressure_u, ci_Temperature, ci_Temperature_u, ci_Weight, ci_Weight_u, ci_APTTSTND, ci_APTTSTND_u, ci_APTT, ci_APTT_u, ci_ALT, ci_ALT_u, ci_ALB, ci_ALB_u, ci_ALP, ci_ALP_u, ci_AMYLASE, ci_AMYLASE_u, ci_AST, ci_AST_u, ci_BASEEXCS, ci_BASEEXCS_u, ci_BASO, ci_BASO_u, ci_BASOLE, ci_BASOLE_u, ci_BICARB, ci_BICARB_u, ci_BILI, ci_BILI_u, ci_CRP, ci_CRP_u, ci_CA, ci_CA_u, ci_CAION, ci_CAION_u, ci_CAIONPH, ci_CAIONPH_u, ci_CO2, ci_CO2_u, ci_CARBXHGB, ci_CARBXHGB_u, ci_CL, ci_CL_u, ci_CHOL, ci_CHOL_u, ci_CK, ci_CK_u, ci_CREAT, ci_CREAT_u, ci_DDIMER, ci_DDIMER_u, ci_HGBDOXY, ci_HGBDOXY_u, ci_BILDIR, ci_BILDIR_u, ci_EOS, ci_EOS_u, ci_EOSLE, ci_EOSLE_u, ci_MCHC, ci_MCHC_u, ci_MCH, ci_MCH_u, ci_MCV, ci_MCV_u, ci_ESR, ci_ESR_u, ci_RBC, ci_RBC_u, ci_RDW, ci_RDW_u, ci_FERRITIN, ci_FERRITIN_u, ci_FIBRINO, ci_FIBRINO_u, ci_FIBRINOF, ci_FIBRINOF_u, ci_FIO2, ci_FIO2_u, ci_GGT, ci_GGT_u, ci_GLUC, ci_GLUC_u, ci_HCT, ci_HCT_u, ci_HGB, ci_HGB_u, ci_HBA1C, ci_HBA1C_u, ci_INTLK6, ci_INTLK6_u, ci_IRON, ci_IRON_u, ci_LDH, ci_LDH_u, ci_LACTICAC, ci_LACTICAC_u, ci_WBC, ci_WBC_u, ci_LYM, ci_LYM_u, ci_LYMLE, ci_LYMLE_u, ci_MG, ci_MG_u, ci_MPV, ci_MPV_u, ci_HGBMET, ci_HGBMET_u, ci_MONO, ci_MONO_u, ci_MONOLE, ci_MONOLE_u, ci_NEUT, ci_NEUT_u, ci_NEUTLE, ci_NEUTLE_u, ci_OXYSAT, ci_OXYSAT_u, ci_HGBOXY, ci_HGBOXY_u, ci_PO2FIO2, ci_PO2FIO2_u, ci_PCO2, ci_PCO2_u, ci_PO2, ci_PO2_u, ci_PLATHCT, ci_PLATHCT_u, ci_PLAT, ci_PLAT_u, ci_K, ci_K_u, ci_PCT, ci_PCT_u, ci_PROT, ci_PROT_u, ci_INR, ci_INR_u, ci_PT, ci_PT_u, ci_PTAC, ci_PTAC_u, ci_SODIUM, ci_SODIUM_u, ci_TT, ci_TT_u, ci_TROPONIN, ci_TROPONIN_u, ci_TROPONI, ci_TROPONI_u, ci_TROPONT, ci_TROPONT_u, ci_URATE, ci_URATE_u, ci_UREAN, ci_UREAN_u, ci_PH, ci_PH_u, ci_in_trt, ci_in_modify, ci_in_cat, ci_sa_complications, ci_NLR float, ci_MAP float)
+            Timestamp, usubjid integer, day integer, ci_ho_type, ci_ds_decod, ci_Body_Mass_Index, ci_Body_Mass_Index_u, ci_Diastolic_Blood_Pressure, ci_Diastolic_Blood_Pressure_u, ci_Heart_Rate, ci_Heart_Rate_u, ci_Height, ci_Height_u, ci_Mean_Arterial_Pressure, ci_Mean_Arterial_Pressure_u, ci_Mid_Upper_Arm_Circumference, ci_Mid_Upper_Arm_Circumference_u, ci_Oxygen_Saturation, ci_Oxygen_Saturation_u, ci_Pulse_Rate, ci_Pulse_Rate_u, ci_Respiratory_Rate, ci_Respiratory_Rate_u, ci_Systolic_Blood_Pressure, ci_Systolic_Blood_Pressure_u, ci_Temperature, ci_Temperature_u, ci_Weight, ci_Weight_u, ci_APTTSTND, ci_APTTSTND_u, ci_APTT, ci_APTT_u, ci_ALT, ci_ALT_u, ci_ALB, ci_ALB_u, ci_ALP, ci_ALP_u, ci_AMYLASE, ci_AMYLASE_u, ci_AST, ci_AST_u, ci_BASEEXCS, ci_BASEEXCS_u, ci_BASO, ci_BASO_u, ci_BASOLE, ci_BASOLE_u, ci_BICARB, ci_BICARB_u, ci_BILI, ci_BILI_u, ci_CRP, ci_CRP_u, ci_CA, ci_CA_u, ci_CAION, ci_CAION_u, ci_CAIONPH, ci_CAIONPH_u, ci_CO2, ci_CO2_u, ci_CARBXHGB, ci_CARBXHGB_u, ci_CL, ci_CL_u, ci_CHOL, ci_CHOL_u, ci_CK, ci_CK_u, ci_CREAT, ci_CREAT_u, ci_DDIMER, ci_DDIMER_u, ci_HGBDOXY, ci_HGBDOXY_u, ci_BILDIR, ci_BILDIR_u, ci_EOS, ci_EOS_u, ci_EOSLE, ci_EOSLE_u, ci_MCHC, ci_MCHC_u, ci_MCH, ci_MCH_u, ci_MCV, ci_MCV_u, ci_ESR, ci_ESR_u, ci_RBC, ci_RBC_u, ci_RDW, ci_RDW_u, ci_FERRITIN, ci_FERRITIN_u, ci_FIBRINO, ci_FIBRINO_u, ci_FIBRINOF, ci_FIBRINOF_u, ci_FIO2, ci_FIO2_u, ci_GGT, ci_GGT_u, ci_GLUC, ci_GLUC_u, ci_HCT, ci_HCT_u, ci_HGB, ci_HGB_u, ci_HBA1C, ci_HBA1C_u, ci_INTLK6, ci_INTLK6_u, ci_IRON, ci_IRON_u, ci_LDH, ci_LDH_u, ci_LACTICAC, ci_LACTICAC_u, ci_WBC, ci_WBC_u, ci_LYM, ci_LYM_u, ci_LYMLE, ci_LYMLE_u, ci_MG, ci_MG_u, ci_MPV, ci_MPV_u, ci_HGBMET, ci_HGBMET_u, ci_MONO, ci_MONO_u, ci_MONOLE, ci_MONOLE_u, ci_NEUT, ci_NEUT_u, ci_NEUTLE, ci_NEUTLE_u, ci_OXYSAT, ci_OXYSAT_u, ci_HGBOXY, ci_HGBOXY_u, ci_PO2FIO2, ci_PO2FIO2_u, ci_PCO2, ci_PCO2_u, ci_PO2, ci_PO2_u, ci_PLATHCT, ci_PLATHCT_u, ci_PLAT, ci_PLAT_u, ci_K, ci_K_u, ci_PCT, ci_PCT_u, ci_PROT, ci_PROT_u, ci_INR, ci_INR_u, ci_PT, ci_PT_u, ci_PTAC, ci_PTAC_u, ci_SODIUM, ci_SODIUM_u, ci_TT, ci_TT_u, ci_TROPONIN, ci_TROPONIN_u, ci_TROPONI, ci_TROPONI_u, ci_TROPONT, ci_TROPONT_u, ci_URATE, ci_URATE_u, ci_UREAN, ci_UREAN_u, ci_PH, ci_PH_u, ci_in_trt, ci_in_modify, ci_in_cat, ci_sa_complications, ci_NLR float, ci_MAP float, OxygenTreatment, Status, PulmonaryComplication, CardiacComplication, VascularComplication, AntiCoagulation, AntiInflammatory, AntiViral)
         ''')
         #commit the change to the db
         conn.commit()
@@ -120,29 +120,72 @@ def update_tables(db_file, json_data):
             value_baseline_status = str(item.get('BaselineStatus'))
             value_stay_duration = str(item.get('DurationOfStay'))
             value_oxygen = str(item.get('OxygenTreatment'))
+			
             print(sql, value_gender, value_age, value_pulmonary, value_covid_status, value_final_status, value_icu, value_baseline_status, value_stay_duration, value_oxygen)
+			
             cursor.execute(sql, (value_gender, value_age, value_pulmonary, value_covid_status, value_final_status, value_icu, value_baseline_status, value_stay_duration, value_oxygen,))
             conn.commit()
-            
-            #insert time series records into SQL
+			
+            cursor.execute("SELECT last_insert_rowid()")
+            last_id = cursor.fetchone()
+            print("last_id ", int(last_id[0]))   
+            #insert time series int(weight[0])records into SQL
             for inner_item in item.get('TimeSeries'):
-                sql = "INSERT INTO timeSeries (ci_Respiratory_Rate_u, ci_Heart_Rate_u, ci_Oxygen_Saturation_u, ci_PLAT_u, ci_NEUT_u, ci_LDH, ci_PO2, ci_CREAT, ci_sa_complications) VALUES (?,?,?,?,?,?,?,?,?)"
+                sql = "INSERT INTO timeSeries (Timestamp, usubjid, ci_Heart_Rate, ci_Heart_Rate_u, ci_TROPONIN, ci_TROPONIN_u, ci_Diastolic_Blood_Pressure, ci_Diastolic_Blood_Pressure_u, ci_Systolic_Blood_Pressure, ci_Systolic_Blood_Pressure_u, ci_CRP, ci_CRP_u, ci_LDH, ci_LDH_u, ci_LYM, ci_LYM_u, ci_NEUT, ci_NEUT_u, ci_Oxygen_Saturation, ci_Oxygen_Saturation_u, ci_PO2, ci_PO2_u, ci_PCO2, ci_PCO2_u, ci_Respiratory_Rate, ci_Respiratory_Rate_u, ci_APTT, ci_APTT_u, ci_PLAT, ci_PLAT_u, ci_DDIMER, ci_DDIMER_u, ci_CREAT, ci_CREAT_u, ci_LACTICAC, ci_LACTICAC_u, ci_HCT, ci_HCT_u, OxygenTreatment, Status, PulmonaryComplication, CardiacComplication, VascularComplication, AntiCoagulation, AntiInflammatory, AntiViral) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
             
-                value_respiratory_rate_u = str(inner_item.get('RespiratoryRateU'))
+                value_timestamp = str(inner_item.get('Timestamp'))
+                value_usubjid = int(last_id[0])
+                value_heart_rate = str(inner_item.get('HeartRate'))
                 value_heart_rate_u = str(inner_item.get('HeartRateU'))
+                value_troponin = str(inner_item.get('Troponin'))
+                value_troponin_u = str(inner_item.get('TroponinU'))
+                value_diastolic_bp = str(inner_item.get('DiastolicBP'))
+                value_diastolic_bp_u = str(inner_item.get('DiastolicBPU'))
+                value_systolic_bp = str(inner_item.get('SystolicBP'))
+                value_systolic_bp_u = str(inner_item.get('SystolicBPU'))
+                value_crp = str(inner_item.get('CRP'))
+                value_crp_u = str(inner_item.get('CRPU'))
+                value_ldh = str(inner_item.get('LDH'))
+                value_ldh_u = str(inner_item.get('LDHU'))
+                value_lymphocytes = str(inner_item.get('Lymphocytes'))
+                value_lymphocytes_u = str(inner_item.get('LymphocytesU'))
+                value_neutrophils = str(inner_item.get('Neutrophils'))
+                value_neutrophils_u = str(inner_item.get('NeutrophilsU'))	
+                value_oxygen_saturation = str(inner_item.get('OxygenSaturation'))
                 value_oxygen_saturation_u = str(inner_item.get('OxygenSaturationU'))
-                value_platelets_u = str(inner_item.get('PlateletsU'))
-                value_neutrophils_u = str(inner_item.get('NeutrophilsU'))
-                value_lhd = str(inner_item.get('LDH'))
                 value_po2 = str(inner_item.get('PO2'))
+                value_po2_u = str(inner_item.get('PO2U'))
+                value_pco2 = str(inner_item.get('PCO2'))
+                value_pco2_u = str(inner_item.get('PCO2U'))
+                value_respiratory_rate = str(inner_item.get('RespiratoryRate'))
+                value_respiratory_rate_u = str(inner_item.get('RespiratoryRateU'))
+                value_aptt = str(inner_item.get('aPTT'))
+                value_aptt_u = str(inner_item.get('aPTTU'))
+                value_platelets = str(inner_item.get('Platelets'))
+                value_platelets_u = str(inner_item.get('PlateletsU'))
+                value_ddimer = str(inner_item.get('DDimer'))
+                value_ddimer_u = str(inner_item.get('DDimerU'))
                 value_creatinine = str(inner_item.get('Creatinine'))
+                value_creatinine_u = str(inner_item.get('CreatinineU'))
+                value_lactate = str(inner_item.get('Lactate'))
+                value_lactate_u = str(inner_item.get('LactateU'))
+                value_hematocrit = str(inner_item.get('Hematocrit'))
+                value_hematocrit_u = str(inner_item.get('HematocritU'))
+                value_oxygen_treatment = str(inner_item.get('OxygenTreatment'))
+                value_status = str(inner_item.get('Status'))
+                value_pulmonary_complication = str(inner_item.get('PulmonaryComplication'))
+                value_cardiac_complication = str(inner_item.get('CardiacComplication'))
                 value_vascular_complication = str(inner_item.get('VascularComplication'))
+                value_anticoagulation = str(inner_item.get('AntiCoagulation'))
+                value_antiinflammatory = str(inner_item.get('AntiInflammatory'))
+                value_antiviral = str(inner_item.get('AntiViral'))
             
-                print(sql, value_respiratory_rate_u, value_heart_rate_u, value_oxygen_saturation_u, value_platelets_u, value_neutrophils_u, value_lhd, value_po2, value_creatinine, value_vascular_complication)
-                cursor.execute(sql, (value_respiratory_rate_u, value_heart_rate_u, value_oxygen_saturation_u, value_platelets_u, value_neutrophils_u, value_lhd, value_po2, value_creatinine, value_vascular_complication,))
+                print(sql, value_timestamp, value_usubjid, value_heart_rate, value_heart_rate_u, value_troponin,value_troponin_u, value_diastolic_bp, value_diastolic_bp_u, value_systolic_bp, value_systolic_bp_u, value_crp, value_crp_u, value_ldh, value_ldh_u, value_lymphocytes, value_lymphocytes_u, value_neutrophils, value_neutrophils_u, value_oxygen_saturation, value_oxygen_saturation_u, value_po2, value_po2_u, value_pco2, value_pco2_u, value_respiratory_rate, value_respiratory_rate_u, value_aptt, value_aptt_u, value_platelets, value_platelets_u, value_ddimer, value_ddimer_u, value_creatinine, value_creatinine_u, value_lactate, value_lactate_u, value_hematocrit, value_hematocrit_u, value_oxygen_treatment, value_status, value_pulmonary_complication, value_cardiac_complication, value_vascular_complication, value_anticoagulation, value_antiinflammatory, value_antiviral)
+				
+                cursor.execute(sql, (value_timestamp, value_usubjid, value_heart_rate, value_heart_rate_u, value_troponin,value_troponin_u, value_diastolic_bp, value_diastolic_bp_u, value_systolic_bp, value_systolic_bp_u, value_crp, value_crp_u, value_ldh, value_ldh_u, value_lymphocytes, value_lymphocytes_u, value_neutrophils, value_neutrophils_u, value_oxygen_saturation, value_oxygen_saturation_u, value_po2, value_po2_u, value_pco2, value_pco2_u, value_respiratory_rate, value_respiratory_rate_u, value_aptt, value_aptt_u, value_platelets, value_platelets_u, value_ddimer, value_ddimer_u, value_creatinine, value_creatinine_u, value_lactate, value_lactate_u, value_hematocrit, value_hematocrit_u, value_oxygen_treatment, value_status, value_pulmonary_complication, value_cardiac_complication, value_vascular_complication, value_anticoagulation, value_antiinflammatory, value_antiviral,))
                 conn.commit()
  
- 
+
         # Output the table if required
         #cursor.execute("SELECT * FROM timeSeries")
         #rows = cursor.fetchall()
