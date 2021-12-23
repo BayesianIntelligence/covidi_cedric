7f5699ecb8597b1ed8e204704f6a523277418be0
ross@bardai.com.au <ross@edcore.com.au>
2021-12-23 15:01:38 +1100

    swapped from GitLab to GitHub (for inner clone

--- clone.py
+++ clone.py
@@ -4,7 +4,11 @@ import sys
 import shutil
 
 GIT_REPO = 'https://gitlab.erc.monash.edu.au/stevenm/covid-intelligence-iddo/'
+# using temporary GIT repo replacement, SWSLHD does not have access to gitlab
+GIT_REPO = "https://github.com/COVID-I/covidi_validation"
 IMPORT_FOLDER = './covid-intelligence-iddo/'
+# using tremporay git repo replacement
+IMPORT_FOLDER = './covidi_validation/'
 
 #clone validation scripts from gitlab
 def clone():
@@ -17,7 +21,7 @@ def clone():
 
     print("copying validation script into repo folder")
 	
-    shutil.copy("validation.py", IMPORT_FOLDER + "validation_copy.py")
+    shutil.copy("validation.py", IMPORT_FOLDER + "validation.py")
     print("setting working directory to: ", IMPORT_FOLDER)
 	
     os.chdir(IMPORT_FOLDER)
